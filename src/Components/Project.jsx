71e122227fb6ccd757a3d20a653773e1a7b69eba
Si0dot7 <felm27010@gmail.com>
2025-01-29 19:57:15 +0700

    update for deploy

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,26 +1,52 @@
 import React from 'react'
-import me from '../assets/me.jpg'
+import current from '../assets/current.png'
+import form from '../assets/form.png'
+import mode from '../assets/mode.png'
+import quiz from '../assets/quiz.png'
+import dash from '../assets/dash.png'
+import web from '../assets/web.png'
 const projects=[
     {
         id: 1,
-        name: "Employee MS",
-        technologies: "MERN Stack",
-        image: me,
-        github: "https://github.com/YouafKhan1",
+        name: "Currency Inverter",
+        technologies: "React.js CSS with API",
+        image: current,
+        github: "https://github.com/Si0dot7/Currency-Inverter-useAPI-.git",
       },
       {
         id: 2,
-        name: "Blog App",
-        technologies: "MERN Stack",
-        image: me,
-        github: "https://github.com/YouafKhan1",
+        name: "Form Register",
+        technologies: "React.js CSS",
+        image: form,
+        github: "https://github.com/Si0dot7/FormValidation.git",
       },
       {
         id: 3,
-        name: "Book MS",
-        technologies: "MERN Stack",
-        image: me,
-        github: "https://github.com/YouafKhan1",
+        name: "Dark & Light Mode",
+        technologies: "React.js CSS",
+        image: mode,
+        github: "https://github.com/Si0dot7/SwitcMode.git",
+      },
+      {
+        id: 4,
+        name: "Quiz Game",
+        technologies: "React.js CSS",
+        image: quiz,
+        github: "https://github.com/Si0dot7/quiz-test.git",
+      },
+      {
+        id: 5,
+        name: "Website Show API-Dashboard",
+        technologies: "React.js CSS with API and Apexcharts",
+        image: dash,
+        github: "https://github.com/Si0dot7/1344_Front-end.git",
+      },
+      {
+        id: 6,
+        name: "Portfolio",
+        technologies: "React.js(vite) Tailwind.css and Responsive Web Design",
+        image: web,
+        github: "https://github.com/Si0dot7/Portfolio.git",
       },
 ];
 
@@ -28,7 +54,7 @@ const Project = () => {
   return (
     <div className='bg-black text-white py-20' id='project'>
         <div className='container mx-auto px-8 md:px-16 lg:px-24'>
-            <h2 className='text-4xl font-bold text-center mb-12'>My Project</h2>
+            <h2 className='text-4xl font-bold text-center mb-12'>My Projects</h2>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
                 {projects.map((project)=>{
                     return(
