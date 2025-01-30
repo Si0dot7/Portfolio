52a5df289595600a8a532ce617d4ae5b34ed9b72
Si0dot7 <felm27010@gmail.com>
2025-01-30 17:12:20 +0700

    update edu

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
             <div className='space-x-7'>
                 <a href='#home' className='hover:text-gray-400'>Home</a>
                 <a href='#about' className='hover:text-gray-400'>About Me</a>
-                <a href='#service' className='hover:text-gray-400'>Service</a>
+                <a href='#service' className='hover:text-gray-400'>Education</a>
                 <a href='#project' className='hover:text-gray-400'>Project</a>
                 <a href='#contact' className='hover:text-gray-400'>Contact</a>
             </div>
